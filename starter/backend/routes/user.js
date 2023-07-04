aa5692f77e62a4f2c843743842396c415c2ce5d4
Rishav <ahoul72@gmail.com>
2023-07-04 11:22:24 +0530

    Created register, login, logout functionality

---
+++ starter/backend/routes/user.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  register,
+  activateAccount,
+  login,
+  auth,
+  sendVerification,
+} = require("../controllers/user");
+const { authUser } = require("../middlewares/auth");
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/activate", authUser, activateAccount);
+router.post("/login", login);
+router.post("/sendVerification", sendVerification);
+
+module.exports = router;
---
+++ starter/backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express"); //initializing express.js
+const mongoose = require("mongoose");
+const cors = require("cors"); //initializing cors
+const { readdirSync } = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors()); //injecting cors to config with our server
+
+//routes
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+//database
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("database connected successfully"))
+  .catch((err) => console.log("error in connecting to mongodb", err));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
